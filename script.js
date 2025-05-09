78d09beaa4306a9054faaf49bf3631431f0368b8
Devesh Vyas <medeveshvyas@gmail.com>
2025-05-09 16:32:03 +0530

    songs list added into library section

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 console.log('Let Start JS');
 
-async function main() {
-    
-    let a = await fetch("http://192.168.1.8:3000/songs/")
-        let response = await a.text();
-        console.log(response);
-        let div = document.createElement("div")
+async function getSongs() {
 
+    let a = await fetch("http://192.168.1.5:3000/songs/")
+    let response = await a.text();
+    // console.log(response);
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1])
+        }
+    }
+    return songs
 }
+
+async function main() {
+    let songs = await getSongs()
+    console.log(songs);
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs){
+        songUL.innerHTML = songUL.innerHTML + `<li> 
+       
+                        <img src="img/icons/music.svg" alt="music">
+                        <div class="info">
+                            <div>${song.replaceAll("%20", " ")}</div>
+                            <div>Devesh</div>
+                        </div>
+                        <div class="playnow">
+                            <span>Play Now</span>
+                            <img src="img/icons/play.svg" alt="">
+                        </div>
+                    
+         </li>`;
+    }
+
+    // play the first song
+    var audio = new Audio(songs[1])
+    // audio.play();
+}
+
 main()
+
