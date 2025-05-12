db661a87204e1c299286f7e07c712d960665c10c
Devesh Vyas <46879927+deveshvyas1@users.noreply.github.com>
2025-05-12 16:49:17 +0530

    Update script.js for song library not show

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let currentSong = new Audio();
 
 async function getSongs() {
 
-    let a = await fetch("/songs/")
+    let a = await fetch("songs/")
     let response = await a.text();
     // console.log(response);
     let div = document.createElement("div")
