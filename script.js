0d48c5259e0f7613521f8d5696a55010c9c242cb
Devesh Vyas <medeveshvyas@gmail.com>
2025-05-13 23:05:33 +0530

    remove local files

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ function secondToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://192.168.1.9:3000/${folder}/`)
+    // let a = await fetch(`http://192.168.1.9:3000/${folder}/`)
+    let a = await fetch(`${folder}`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -69,7 +70,8 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://192.168.1.9:3000/songs/`)
+    // let a = await fetch(`http://192.168.1.9:3000/songs/`)
+    let a = await fetch(`songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -81,7 +83,8 @@ async function displayAlbums() {
         if (e.href.includes("/songs")) {
             let folder = e.href.split("/").slice(-2)[0]
             // get the metadata of the folder
-            let a = await fetch(`http://192.168.1.9:3000/songs/${folder}/info.json`)
+            // let a = await fetch(`http://192.168.1.9:3000/songs/${folder}/info.json`)
+            let a = await fetch(`songs/${folder}/info.json`)
             let response = await a.json();
             console.log(response);
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="artist">
